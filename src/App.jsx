38f9c5601b124516074db5556fe5841305b8387f
zura tetrashvili <zuratetra@gmail.com>
2024-06-13 20:16:47 +0400

    create new project, add project style, add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
-import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
+  const buttonsText = ["Small Business", "Medium Business", "Enterprise"];
 
   return (
     <>
-      <div className="">test</div>
+      <div className="">
+        <h1 className="font-bold text-[64px] text-center">
+          Easy Turn-Key Integration
+        </h1>
+        <p className="max-w-[779px] text-center mt-6 text-lg">
+          Increase job satisfaction, improve engagement, decrease burnout and
+          lower payroll liability by reimagining what employees can do with
+          their PTO.
+        </p>
+        <div className="flex justify-center mt-[50px]">
+          {buttonsText.map((btn, index) => (
+            <button
+              key={index}
+              className="bg-purpleDark px-[18px] h-[38px] text-white font-bold text-xs flex items-center border border-white focus:outline-none focus:border-none"
+            >
+              {btn}
+            </button>
+          ))}
+          <img src="/images/rame.svg" alt="" />
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purpleDark: "#9D71FD",
+      },
+    },
   },
   plugins: [],
 };
